65394fcabdbba74e736bf5a49351f0a4580d4957
Shashank Dwivedi <shashankdwivedi9648@gmail.com>
2025-05-20 18:47:55 +0530

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link';
+import { FaDownload } from 'react-icons/fa';
 
 const Hero = () => {
   return (
     <section id="hero" className="container mx-auto min-h-[calc(100vh-80px)] flex flex-col items-center justify-center text-center py-12 px-4 md:px-8">
       <div className="space-y-6 mb-10 md:mb-0 max-w-3xl">
-        <h2 className="text-2xl md:text-[32px] font-bold text-white/80 tracking-[.09em] uppercase font-poppins">
+        <h2 className="text-2xl md:text-[32px] font-bold text-neutral-400 tracking-[.09em] uppercase font-poppins">
           Hi, my name is
         </h2>
         <h1 className="text-4xl sm:text-5xl md:text-[72px] font-bold font-sora">
@@ -15,8 +16,8 @@ const Hero = () => {
         <h2 className="text-3xl sm:text-4xl md:text-[72px] font-bold font-sora">
           I am a Software Engineer
         </h2>
-        <p className="text-xl md:text-[28px] text-white font-sora max-w-xl mx-auto">
-          3+ years of making the job done in the Tech industry! I am a product-oriented Software Engineer with a business mindset from bringing the latest tech to launching successful startups!
+        <p className="text-xl md:text-[28px] text-white font-sora max-w-xl mx-auto leading-relaxed">
+          With 3+ years in tech, I&apos;m a product-focused Software Engineer. I leverage the latest technology and a business mindset to launch successful startups.
         </p>
         <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 justify-center pt-5">
           <Link
@@ -30,9 +31,10 @@ const Hero = () => {
             download
             target="_blank"
             rel="noopener noreferrer"
-            className="border border-[#00A3FF] hover:bg-[#00A3FF] hover:text-[#00040F] text-[#00A3FF] font-sora font-bold py-4 px-8 sm:py-5 sm:px-10 md:py-6 md:px-10 rounded-[5px] text-lg sm:text-xl md:text-[24px] shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105"
+            className="border border-[#00A3FF] hover:bg-[#00A3FF] hover:text-[#00040F] text-[#00A3FF] font-sora font-bold py-4 px-8 sm:py-5 sm:px-10 md:py-6 md:px-10 rounded-[5px] text-lg sm:text-xl md:text-[24px] shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 flex items-center justify-center space-x-2"
           >
-            Download Resume
+            <FaDownload />
+            <span>Download Resume</span>
           </a>
         </div>
       </div>
